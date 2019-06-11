511591f8471b8b9845b76e6f04715631b6ae0811
Alessandro Di Maria <adm@m42.ch>
2019-06-11 21:55:25 +0200

    run as docker swarm

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -4,11 +4,20 @@ set -e
 APP_NAME=thundermoon
 IMAGE=grrrocker/thundermoon-umbrella:$TAG
 
-echo "Deploying $APP_NAME..."
-
-echo " * Pull image $IMAGE..."
+echo "pull image $IMAGE..."
 docker pull $IMAGE
 
 cd /srv/sites/thundermoon
 
-IMAGE=$IMAGE docker-compose up
+if docker node ls > /dev/null 2>&1; then
+  echo "swarm already initialized"
+else
+  echo "docker swarm initializing.."
+  docker swarm init
+fi
+
+echo "stack deploy"
+IMAGE=$IMAGE docker stack deploy --with-registry-auth -c docker-compose.yml --prune $APP_NAME
+
+echo " docker prune..."
+docker system prune -af
