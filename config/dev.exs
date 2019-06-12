de6ef0cc23d72cc9f0646243153494502f77b21d
Alessandro Di Maria <adm@m42.ch>
2019-06-12 17:37:06 +0200

    remove webpack

--- config/dev.exs
+++ config/dev.exs
@@ -21,13 +21,13 @@ config :thundermoon_web, ThundermoonWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../apps/thundermoon_web/assets", __DIR__)
-    ]
+    # node: [
+    #   "node_modules/webpack/bin/webpack.js",
+    #   "--mode",
+    #   "development",
+    #   "--watch-stdin",
+    #   cd: Path.expand("../apps/thundermoon_web/assets", __DIR__)
+    # ]
   ]
 
 # ## SSL Support
