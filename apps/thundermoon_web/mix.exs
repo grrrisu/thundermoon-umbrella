c2368b88a6d5dcf385a87fb663fb1c0c6f6528a7
Alessandro Di Maria <adm@m42.ch>
2019-06-16 21:28:17 +0200

    start plugsnag and bugsnag explicitly, maybe because we are in an umbrella app

--- apps/thundermoon_web/mix.exs
+++ apps/thundermoon_web/mix.exs
@@ -24,7 +24,7 @@ defmodule ThundermoonWeb.MixProject do
   def application do
     [
       mod: {ThundermoonWeb.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :plugsnag, :bugsnag]
     ]
   end
 
