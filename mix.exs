30db67ba0507556f697077c28b74d05873eaabbe
Alessandro Di Maria <adm@m42.ch>
2019-06-16 18:36:35 +0200

    add bugsnag via plugsnag

--- mix.exs
+++ mix.exs
@@ -22,6 +22,6 @@ defmodule Thundermoon.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:plugsnag, "~> 1.3.0"}]
   end
 end
