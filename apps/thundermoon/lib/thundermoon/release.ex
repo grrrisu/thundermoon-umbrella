db1af8e31730144edb36401324f640fd5c8068c8
Alessandro Di Maria <adm@m42.ch>
2019-07-29 20:02:12 +0200

    make migrations available for releases

---
+++ apps/thundermoon/lib/thundermoon/release.ex
@@ -0,0 +1,22 @@
+defmodule Thundermoon.Release do
+  @app :thundermoon
+
+  def migrate do
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  def seeds do
+    IO.puts("todo!")
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
