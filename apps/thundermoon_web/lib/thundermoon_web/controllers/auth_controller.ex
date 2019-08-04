94b2f62c7df3a77121ad778270b535aad030bf9c
Alessandro Di Maria <adm@m42.ch>
2019-08-04 13:55:32 +0200

    refactore auth_service

--- apps/thundermoon_web/lib/thundermoon_web/controllers/auth_controller.ex
+++ apps/thundermoon_web/lib/thundermoon_web/controllers/auth_controller.ex
@@ -13,15 +13,13 @@ defmodule ThundermoonWeb.AuthController do
 
   def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
     conn
-    |> put_flash(:error, "Authentification failed")
+    |> put_flash(:error, "Authentification failed!")
     |> redirect(to: "/")
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     case AuthService.find_or_create(auth) do
       {:ok, user} ->
-        IO.inspect(user)
-
         conn
         |> put_flash(:info, "Successfully authenticated as #{user.username}.")
         |> put_session(:current_user, user)
