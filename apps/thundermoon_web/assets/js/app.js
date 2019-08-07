b4652fcf2ef834b4fdfcbc6d5a1346b5a4b8ab54
Alessandro Di Maria <adm@m42.ch>
2019-08-07 20:06:51 +0200

    fix js codestyle

--- apps/thundermoon_web/assets/js/app.js
+++ apps/thundermoon_web/assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.scss"
+import css from "../css/app.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,12 +9,12 @@ import css from "../css/app.scss"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
-import LiveSocket from "phoenix_live_view"
+import LiveSocket from "phoenix_live_view";
 
-let liveSocket = new LiveSocket("/live")
-liveSocket.connect()
+let liveSocket = new LiveSocket("/live");
+liveSocket.connect();
 
 // Import local files
 //
