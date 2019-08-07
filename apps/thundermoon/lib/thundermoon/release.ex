e2446984c6f94d8efdc88a1815f1cf699ea57852
Alessandro Di Maria <adm@m42.ch>
2019-08-07 20:44:09 +0200

    fix code styles

--- apps/thundermoon/lib/thundermoon/release.ex
+++ apps/thundermoon/lib/thundermoon/release.ex
@@ -1,14 +1,16 @@
 defmodule Thundermoon.Release do
   @app :thundermoon
 
+  alias Ecto.Migrator
+
   def migrate do
     for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      {:ok, _, _} = Migrator.with_repo(repo, &Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
-    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+    {:ok, _, _} = Migrator.with_repo(repo, &Migrator.run(&1, :down, to: version))
   end
 
   def seeds do
