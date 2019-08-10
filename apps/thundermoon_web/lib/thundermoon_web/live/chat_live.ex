75ef5204c058b4da1773fe26c8d86ee20e066fbf
Alessandro Di Maria <adm@m42.ch>
2019-08-10 22:48:22 +0200

    send a message

--- apps/thundermoon_web/lib/thundermoon_web/live/chat_live.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/chat_live.ex
@@ -6,10 +6,21 @@ defmodule ThundermoonWeb.ChatLive do
 
   def mount(session, socket) do
     user = Repo.get!(User, session[:current_user_id])
-    {:ok, assign(socket, %{current_user: user})}
+    messages = []
+    {:ok, assign(socket, %{current_user: user, messages: messages})}
   end
 
   def render(assigns) do
     ThundermoonWeb.ChatView.render("index.html", assigns)
+  end
+
+  def handle_event("send", %{"message" => %{"text" => text}}, socket) do
+    new_message = %{user: username(socket), text: text}
+    messages = [new_message | socket.assigns.messages]
+    {:noreply, assign(socket, %{messages: messages})}
+  end
+
+  defp username(socket) do
+    socket.assigns.current_user.username
   end
 end
