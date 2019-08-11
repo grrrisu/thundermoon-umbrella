4d0895599e54db3fb5cc796f7eaae4b81069b325
Alessandro Di Maria <adm@m42.ch>
2019-08-11 11:34:49 +0200

    extend auth support to create users on the fly

--- apps/thundermoon_web/test/support/auth_support.ex
+++ apps/thundermoon_web/test/support/auth_support.ex
@@ -1,7 +1,15 @@
 defmodule ThundermoonWeb.AuthSupport do
   import Plug.Test
 
-  def login_as(conn, user) do
+  alias Thundermoon.Factory
+  alias Thundermoon.Accounts.User
+
+  def login_as(conn, %User{} = user) do
     init_test_session(conn, current_user_id: user.id)
+  end
+
+  def login_as(conn, %{} = user_params) do
+    current_user = Factory.create(:user, user_params)
+    login_as(conn, current_user)
   end
 end
