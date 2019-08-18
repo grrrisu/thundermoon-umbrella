534339ca2c5a47a47bebf016381c7c0b6eb3d263
Alessandro Di Maria <adm@m42.ch>
2019-08-18 22:09:06 +0200

    extract chat spec to its own file

---
+++ apps/thundermoon_web/test/cypress/integration/chat_spec.js
@@ -0,0 +1,20 @@
+describe('chat page', function() {
+
+  beforeEach(function() {
+    cy.visit("/auth/integration?external_user_id=123");
+  });
+
+  afterEach(function() {
+    // clear all messages
+    cy.contains('Logout').click();
+  })
+
+  it('send a chat message', function() {
+    cy.visit("/chat");
+    cy.get("input#message_text").type("hello everyone!");
+    cy.get("form").submit();
+    cy.get("#messages").contains("crumb");
+    cy.get("#messages").contains("hello everyone!");
+  })
+});
+
--- apps/thundermoon_web/test/cypress/integration/page_spec.js
+++ apps/thundermoon_web/test/cypress/integration/page_spec.js
@@ -20,24 +20,3 @@ describe('dashboard page', function() {
     cy.contains('Welcome crumb');
   })
 });
-
-describe('chat page', function() {
-
-  beforeEach(function() {
-    cy.visit("/auth/integration?external_user_id=123");
-  });
-
-  afterEach(function() {
-    // clear all messages
-    cy.contains('Logout').click();
-  })
-
-  it('send a chat message', function() {
-    cy.visit("/chat");
-    cy.get("input#message_text").type("hello everyone!");
-    cy.get("form").submit();
-    cy.get("#messages").contains("crumb");
-    cy.get("#messages").contains("hello everyone!");
-  })
-});
-
