dbb59b59d772281475c50b0e4205ac229c854482
Alessandro Di Maria <adm@m42.ch>
2019-08-18 21:56:35 +0200

    move cypress foler structure from assets to test

--- apps/thundermoon_web/test/cypress/integration/page_spec.js
+++ apps/thundermoon_web/test/cypress/integration/page_spec.js
@@ -1,6 +1,6 @@
 describe('root page', function() {
   it('should see the title', function() {
-    cy.visit("http://localhost:4000");
+    cy.visit("/");
     cy.contains('Thundermoon');
   })
 });
@@ -8,7 +8,7 @@ describe('root page', function() {
 describe('dashboard page', function() {
 
   beforeEach(function() {
-    cy.visit("http://localhost:4000/auth/integration?external_user_id=123");
+    cy.visit("/auth/integration?external_user_id=123");
   });
 
   afterEach(function() {
@@ -16,8 +16,28 @@ describe('dashboard page', function() {
   })
 
   it('should see the title', function() {
-
-    cy.visit("http://localhost:4000/dashboard");
+    cy.visit("/dashboard");
     cy.contains('Welcome crumb');
   })
 });
+
+describe('chat page', function() {
+
+  beforeEach(function() {
+    cy.visit("/auth/integration?external_user_id=123");
+  });
+
+  afterEach(function() {
+    // clear all messages
+    cy.contains('Logout').click();
+  })
+
+  it('send a chat message', function() {
+    cy.visit("/chat");
+    cy.get("input#message_text").type("hello everyone!");
+    cy.get("form").submit();
+    cy.get("#messages").contains("crumb");
+    cy.get("#messages").contains("hello everyone!");
+  })
+});
+
