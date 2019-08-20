13d494b16203d21aa51637f535f2cb5ba89f4c50
Alessandro Di Maria <adm@m42.ch>
2019-08-20 23:38:53 +0200

    log events not catched by plug

--- apps/thundermoon_web/lib/thundermoon_web/application.ex
+++ apps/thundermoon_web/lib/thundermoon_web/application.ex
@@ -19,6 +19,7 @@ defmodule ThundermoonWeb.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ThundermoonWeb.Supervisor]
+    {:ok, _} = Logger.add_backend(Sentry.LoggerBackend)
     Supervisor.start_link(children, opts)
   end
 
