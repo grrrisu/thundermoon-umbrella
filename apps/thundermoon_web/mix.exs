6d214f3daf8d8a9247c2291c13784c418f5a401a
Alessandro Di Maria <adm@m42.ch>
2019-08-21 23:29:57 +0200

    move all deps from umbrella to apps

--- apps/thundermoon_web/mix.exs
+++ apps/thundermoon_web/mix.exs
@@ -51,6 +51,8 @@ defmodule ThundermoonWeb.MixProject do
       {:ueberauth_github, github: "ueberauth/ueberauth_github"},
       {:canada, "~> 1.0.1"},
       {:canary, "~> 1.1.1"},
+      {:sentry, "~> 7.0"},
+      {:observer_cli, "~> 1.5"},
       {:phoenix_live_reload, "~> 1.2", only: :dev}
     ]
   end
