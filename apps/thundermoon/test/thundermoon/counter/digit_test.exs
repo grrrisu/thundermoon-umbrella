4de04eadecd8c66a829af56309548aed9a3e8564
Alessandro Di Maria <adm@m42.ch>
2019-09-08 15:09:03 +0200

    test digit inc and dec

---
+++ apps/thundermoon/test/thundermoon/counter/digit_test.exs
@@ -0,0 +1,49 @@
+defmodule Thundermoon.DigitTest do
+  use ExUnit.Case, async: true
+
+  alias Thundermoon.Digit
+
+  setup do
+    {:ok, digit_0} = Digit.start(self(), :digit_10, 0)
+    {:ok, digit_9} = Digit.start(self(), :digit_10, 9)
+
+    on_exit(fn ->
+      :ok = Agent.stop(digit_0)
+      :ok = Agent.stop(digit_9)
+    end)
+
+    %{digit_0: digit_0, digit_9: digit_9}
+  end
+
+  test "start with value 5" do
+    assert {:ok, digit} = Digit.start(nil, :digit_10, 5)
+    assert Digit.get(digit) == 5
+    assert :ok = Agent.stop(digit)
+  end
+
+  test "inc by 1", %{digit_0: digit} do
+    assert Digit.get(digit) == 0
+    assert :ok = Digit.inc(digit)
+    assert Digit.get(digit) == 1
+  end
+
+  test "dec by 1", %{digit_9: digit} do
+    assert Digit.get(digit) == 9
+    assert :ok = Digit.dec(digit)
+    assert Digit.get(digit) == 8
+  end
+
+  test "inc overflow", %{digit_9: digit} do
+    assert Digit.get(digit) == 9
+    assert :ok = Digit.inc(digit)
+    assert_received {:overflow, [:digit_10, :inc]}
+    assert Digit.get(digit) == 0
+  end
+
+  test "dec overflow", %{digit_0: digit} do
+    assert Digit.get(digit) == 0
+    assert :ok = Digit.dec(digit)
+    assert_received {:overflow, [:digit_10, :dec]}
+    assert Digit.get(digit) == 0
+  end
+end
