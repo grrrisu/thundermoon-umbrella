f4812f7c201b828b3f447ba03dc04292c5bea4c8
Alessandro Di Maria <adm@m42.ch>
2019-09-18 21:24:13 +0200

    separate set current user and check for member area

--- apps/thundermoon_web/lib/thundermoon_web/plugs/auth_plug.ex
+++ apps/thundermoon_web/lib/thundermoon_web/plugs/auth_plug.ex
@@ -1,6 +1,5 @@
 defmodule ThundermoonWeb.AuthPlug do
   import Plug.Conn
-  import Phoenix.Controller, only: [redirect: 2, put_flash: 3]
 
   alias Thundermoon.Repo
   alias Thundermoon.Accounts.User
@@ -15,15 +14,10 @@ defmodule ThundermoonWeb.AuthPlug do
       user = user_id && Repo.get(User, user_id) ->
         conn
         |> assign(:current_user, user)
-        |> assign(:signed_in, true)
 
       true ->
         conn
         |> assign(:current_user, nil)
-        |> assign(:signed_in, false)
-        |> halt
-        |> put_flash(:error, "Not authorized! Please login first.")
-        |> redirect(to: "/")
     end
   end
 end
