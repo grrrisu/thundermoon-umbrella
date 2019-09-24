bd28556716564eba6ec317f044f5dd363cf9e5ed
Alessandro Di Maria <adm@m42.ch>
2019-09-24 18:18:39 +0200

    game of life simulation

---
+++ apps/thundermoon/test/thundermoon/game_of_life/simulation_test.exs
@@ -0,0 +1,57 @@
+defmodule Thundermoon.GameOfLife.SimulationTest do
+  use ExUnit.Case, async: true
+
+  alias Sim.SeamlessGrid, as: Grid
+  alias Thundermoon.GameOfLife.Simulation
+
+  # https://de.wikipedia.org/wiki/Conways_Spiel_des_Lebens#Die_Spielregeln
+  # https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules
+
+  test "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." do
+    grid = [
+      [false, false, false],
+      [false, false, false],
+      [true, true, true]
+    ]
+
+    grid = Grid.create(3, 3, grid)
+    new_grid = Simulation.sim(grid)
+    assert true == Grid.get(new_grid, 1, 1)
+  end
+
+  test "Any live cell with fewer than two live neighbours dies, as if by underpopulation." do
+    grid = [
+      [false, false, false],
+      [false, true, false],
+      [false, false, true]
+    ]
+
+    grid = Grid.create(3, 3, grid)
+    new_grid = Simulation.sim(grid)
+    assert false == Grid.get(new_grid, 1, 1)
+  end
+
+  test "Any live cell with two or three live neighbours lives on to the next generation." do
+    grid = [
+      [false, false, false],
+      [true, true, false],
+      [false, true, true]
+    ]
+
+    grid = Grid.create(3, 3, grid)
+    new_grid = Simulation.sim(grid)
+    assert true == Grid.get(new_grid, 1, 1)
+  end
+
+  test "Any live cell with more than three live neighbours dies, as if by overpopulation." do
+    grid = [
+      [false, false, true],
+      [true, true, false],
+      [false, true, true]
+    ]
+
+    grid = Grid.create(3, 3, grid)
+    new_grid = Simulation.sim(grid)
+    assert false == Grid.get(new_grid, 1, 1)
+  end
+end
