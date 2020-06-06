06db7f8404d0fff0fea6082e022682f5e3c19924
Alessandro Di Maria <adm@m42.ch>
2020-06-06 13:25:41 +0200

    use ThundermoonWeb, :live_view instead of Phoenix.LiveView to get the new live layout and all needed helpers

--- apps/thundermoon_web/lib/thundermoon_web.ex
+++ apps/thundermoon_web/lib/thundermoon_web.ex
@@ -85,8 +85,7 @@ defmodule ThundermoonWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
-
-      # import ThundermoonWeb.LiveHelpers
+      import ThundermoonWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
---
+++ apps/thundermoon_web/lib/thundermoon_web/live/live_helpers.ex
@@ -0,0 +1,2 @@
+defmodule ThundermoonWeb.LiveHelpers do
+end
