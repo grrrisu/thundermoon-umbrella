55940cd61f3b9a470e2924f4f810477bb93924e5
Alessandro Di Maria <adm@m42.ch>
2020-06-13 14:07:16 +0200

    create sub application `mix new game_of_life —module GameOfLife —sup`

---
+++ apps/game_of_life/mix.exs
@@ -0,0 +1,38 @@
+defmodule GameOfLife.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :game_of_life,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {GameOfLife.Application, []}
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(env) when env in [:test, :integration], do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:sim, in_umbrella: true},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
