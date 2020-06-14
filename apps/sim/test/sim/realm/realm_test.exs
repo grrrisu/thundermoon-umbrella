ad76b603c3edfb6861e1f81a52062a6594e426ed
Alessandro Di Maria <adm@m42.ch>
2020-06-14 15:51:51 +0200

    adapt recovery tests

--- apps/sim/test/sim/realm/realm_test.exs
+++ apps/sim/test/sim/realm/realm_test.exs
@@ -61,8 +61,6 @@ defmodule Sim.RealmTest do
 
     test "stop" do
       assert :ok = Dummy.start_sim()
-      # wait one sim cycle
-      Process.sleep(100)
       assert :ok = Dummy.stop_sim()
       assert_receive({:sim, started: false})
     end
@@ -85,69 +83,70 @@ defmodule Sim.RealmTest do
     end
   end
 
-  # @tag :skip
-  # describe "recover" do
-  #   test "server crash" do
-  #     Realm.set_root(:before_server_crash)
-  #     Realm.start_sim(fn data -> data end)
-  #     Sim.Realm.Server |> GenServer.stop(:shutdown)
-  #     wait_for_all()
-  #     assert :before_server_crash == Sim.Realm.get_root()
-  #     assert true == Sim.Realm.started?()
-  #   end
+  describe "recover" do
+    setup do
+      %{root: Dummy.create(Factory, %{initial: 0})}
+    end
 
-  #   test "simulation loop crash" do
-  #     Realm.set_root(:before_loop_crash)
-  #     Realm.start_sim(fn data -> data end)
-  #     Sim.Realm.SimulationLoop |> GenServer.stop(:shutdown)
-  #     assert_receive({:sim, started: false})
-  #     wait_for_all()
-  #     assert :before_loop_crash == Sim.Realm.get_root()
-  #     assert false == Sim.Realm.started?()
-  #   end
+    test "server crash" do
+      Dummy.start_sim()
+      Dummy.RealmServer |> GenServer.stop(:shutdown)
+      wait_for_all()
+      assert 0 >= Dummy.get_root()
+      assert true == Dummy.started?()
+    end
 
-  #   test "data crash" do
-  #     Realm.set_root(:before_data_crash)
-  #     Realm.start_sim(fn data -> data end)
-  #     Sim.Realm.Data |> Agent.stop(:shutdown)
-  #     wait_for_all()
-  #     refute_receive({:sim, started: false})
-  #     assert nil == Sim.Realm.get_root()
-  #     assert false == Sim.Realm.started?()
-  #   end
+    test "simulation loop crash" do
+      Dummy.start_sim()
+      Dummy.SimulationLoop |> GenServer.stop(:shutdown)
+      assert_receive({:sim, started: false})
+      wait_for_all()
+      assert 0 >= Dummy.get_root()
+      assert false == Dummy.started?()
+    end
 
-  #   test "sim task crash" do
-  #     Realm.set_root(:before_sim_crash)
-  #     Realm.start_sim(fn _ -> raise "sim task crashed" end)
-  #     assert_receive({:sim, started: false})
-  #     wait_for_all()
-  #     assert :before_sim_crash == Sim.Realm.get_root()
-  #     assert false == Sim.Realm.started?()
-  #   end
+    test "data crash" do
+      Dummy.start_sim()
+      Dummy.Data |> Agent.stop(:shutdown)
+      wait_for_all()
+      refute_receive({:sim, started: false})
+      assert nil == Dummy.get_root()
+      assert false == Dummy.started?()
+    end
 
-  #   test "create crash" do
-  #     Realm.set_root(:before_create_crash)
-  #     assert catch_exit(Realm.create(Sim.Grid, nil))
-  #     wait_for_all()
-  #     assert :before_create_crash == Sim.Realm.get_root()
-  #   end
-  # end
+    test "sim task crash" do
+      # negative number will crash sim
+      Dummy.set_root(-1)
+      Dummy.start_sim()
+      # wait one sim cycle
+      Process.sleep(100)
+      assert_receive({:sim, started: false})
+      assert -1 == Dummy.get_root()
+      assert false == Dummy.started?()
+    end
 
-  # def wait_for_all() do
-  #   Enum.all?([Sim.Realm.Server, Sim.Realm.SimulationLoop, Sim.Realm.Data], fn process ->
-  #     wait_for(process)
-  #   end)
-  # end
+    test "create crash" do
+      assert catch_exit(Dummy.create(Factory, :raise))
+      wait_for_all()
+      assert 0 >= Dummy.get_root()
+    end
+  end
 
-  # def wait_for(process) do
-  #   Process.sleep(1)
+  def wait_for_all() do
+    Enum.all?([Dummy.RealmServer, Dummy.SimulationLoop, Dummy.Data], fn process ->
+      wait_for(process)
+    end)
+  end
 
-  #   with pid when is_pid(pid) <- Process.whereis(process),
-  #        true <- Process.alive?(pid) do
-  #     true
-  #   else
-  #     _any ->
-  #       wait_for(process)
-  #   end
-  # end
+  def wait_for(process) do
+    Process.sleep(1)
+
+    with pid when is_pid(pid) <- Process.whereis(process),
+         true <- Process.alive?(pid) do
+      true
+    else
+      _any ->
+        wait_for(process)
+    end
+  end
 end
