a4b2989a60b65bb6f6e7267906767efe1e33b713
Alessandro Di Maria <adm@m42.ch>
2020-07-24 12:22:12 +0200

    update sentry to 8.0

--- apps/thundermoon_web/lib/thundermoon_web/endpoint.ex
+++ apps/thundermoon_web/lib/thundermoon_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule ThundermoonWeb.Endpoint do
+  use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :thundermoon_web
 
   @session_options [
@@ -45,6 +46,7 @@ defmodule ThundermoonWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug Sentry.PlugContext
   plug Plug.MethodOverride
   plug Plug.Head
 
