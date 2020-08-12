d9291d1590852943a9058dc6885ebf79ea605a75
Alessandro Di Maria <adm@m42.ch>
2020-08-12 13:40:05 +0200

    context functions to pass to invitro

--- apps/sim/lib/sim/laboratory.ex
+++ apps/sim/lib/sim/laboratory.ex
@@ -1,8 +1,22 @@
 defmodule Sim.Laboratory do
   alias Sim.Laboratory.Server
 
-  def create(server \\ Server) do
-    GenServer.call(server, :create)
+  def create(create_func, server \\ Server) do
+    {:ok, entry} = GenServer.call(server, :create)
+    {:ok, object} = GenServer.call(entry.pid, {:create, create_func})
+    {entry.id, object}
+  end
+
+  def object(id, server \\ Server) do
+    call(id, :object, server)
+  end
+
+  def start(id, sim_func, server \\ Server) do
+    call(id, {:start, sim_func}, server)
+  end
+
+  def stop(id, server \\ Server) do
+    call(id, :stop, server)
   end
 
   def get(id, server \\ Server) do
@@ -11,6 +25,13 @@ defmodule Sim.Laboratory do
 
   def update(id, key, value, server \\ Server) do
     GenServer.call(server, {:update, id, key, value})
+  end
+
+  def call(id, payload, server \\ Server) do
+    case get(id, server) do
+      {:error, :not_found} -> {:error, :not_found}
+      entry -> GenServer.call(entry.pid, payload)
+    end
   end
 
   def delete(id, server \\ Server) do
