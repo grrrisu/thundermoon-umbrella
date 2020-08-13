29ddab4710ff7525cfc40b4322fe0ac1fad91f14
Alessandro Di Maria <adm@m42.ch>
2020-08-13 12:36:20 +0200

    use alias for nested modules

--- apps/lotka_volterra/test/lotka_volterra_test.exs
+++ apps/lotka_volterra/test/lotka_volterra_test.exs
@@ -2,12 +2,14 @@ defmodule LotkaVolterraTest do
   use ExUnit.Case
   doctest LotkaVolterra
 
+  alias Sim.Laboratory
+
   describe "simulation" do
     test "with default values" do
       {id, vegetation} = LotkaVolterra.create(ThundermoonWeb.PubSub)
       LotkaVolterra.start(id)
       # this would take 1 sec to the first change
-      send(Sim.Laboratory.get(id).pid, :tick)
+      send(Laboratory.get(id).pid, :tick)
       assert vegetation.size < LotkaVolterra.object(id).size
       LotkaVolterra.stop(id)
     end
