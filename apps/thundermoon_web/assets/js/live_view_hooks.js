de5d0e1fb4e36e2114fee9cbfa9801e1376257a6
Alessandro Di Maria <adm@m42.ch>
2020-08-13 18:37:54 +0200

    add chartjs and push data to client

---
+++ apps/thundermoon_web/assets/js/live_view_hooks.js
@@ -0,0 +1,41 @@
+// define hooks for live view
+let Hooks = {};
+
+import Chart from 'chart.js';
+
+Hooks.LotkaVolterraChart = {
+  mounted() {
+    const chart = new Chart(this.el, {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          label: "vegetation",
+          borderColor: "rgba(59, 120, 59, 0.8)",
+          backgroundColor: "rgba(44, 160, 44, 0.2)",
+          lineTension: 0,
+          borderWidth: 2,
+          data: []
+        }]
+      },
+      options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+      }
+    });
+
+    this.handleEvent("update-chart", (data) => {
+      let vegetation = Number(data.vegetation).toFixed(2);
+      chart.data.labels.push(data.x_axe);
+      chart.data.datasets[0].data.push(vegetation);
+      chart.update();
+    });
+  }
+}
+
+export default Hooks;
