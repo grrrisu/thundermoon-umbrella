adc555939d9c4d0fd2c2081993de3a9724dc6095
Alessandro Di Maria <adm@m42.ch>
2020-08-13 11:45:29 +0200

    lotka voltera live_view, start button component, lotka voltera context

--- apps/lotka_volterra/test/lotka_volterra_test.exs
+++ apps/lotka_volterra/test/lotka_volterra_test.exs
@@ -1,4 +1,15 @@
 defmodule LotkaVolterraTest do
   use ExUnit.Case
   doctest LotkaVolterra
+
+  describe "simulation" do
+    test "with default values" do
+      {id, vegetation} = LotkaVolterra.create(ThundermoonWeb.PubSub)
+      LotkaVolterra.start(id)
+      # this would take 1 sec to the first change
+      send(Sim.Laboratory.get(id).pid, :tick)
+      assert vegetation.size < LotkaVolterra.object(id).size
+      LotkaVolterra.stop(id)
+    end
+  end
 end
