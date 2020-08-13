2d2ecc6c7cae388f291b8e876a1772ba1ab2c8ea
Alessandro Di Maria <adm@m42.ch>
2020-08-13 13:47:31 +0200

    remove realm for lotka volterra

--- apps/lotka_volterra/mix.exs
+++ apps/lotka_volterra/mix.exs
@@ -19,8 +19,7 @@ defmodule LotkaVolterra.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {LotkaVolterra.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
