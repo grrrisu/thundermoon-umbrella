71efbfd0a35e70266ae54aa1e74f511e5055d549
Alessandro Di Maria <adm@m42.ch>
2020-08-13 12:26:19 +0200

    add test for lotka volterra live_view and start button component

---
+++ apps/thundermoon_web/test/thundermoon_web/live/shared/start_button_test.exs
@@ -0,0 +1,24 @@
+defmodule ThundermoonWeb.LotkaVolterraIndexTest do
+  use ThundermoonWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  import ThundermoonWeb.AuthSupport
+
+  setup(%{conn: conn}) do
+    conn = login_as(conn, %{username: "crumb"})
+    {:ok, view, _html} = live(conn, "/lotka-volterra")
+    html = render_click(view, "create")
+    %{view: view, html: html}
+  end
+
+  test "click start", %{view: view} do
+    assert view |> element("#start-button", "start") |> render_click()
+  end
+
+  test "click stop", %{view: view} do
+    send(view.pid, {:sim, started: true})
+    html = render(view)
+    assert html =~ "stop"
+    assert view |> element("#start-button", "stop") |> render_click()
+  end
+end
