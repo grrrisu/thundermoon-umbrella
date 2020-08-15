945efb0351843e8819df03f28aa1cec842f0275d
Alessandro Di Maria <alessandro@Grrrbook.local>
2020-08-15 22:48:10 +0200

    Chart component

---
+++ apps/thundermoon_web/lib/thundermoon_web/live/lotka_volterra/chart.ex
@@ -0,0 +1,32 @@
+defmodule ThundermoonWeb.LotkaVolterra.Chart do
+  use ThundermoonWeb, :live_component
+
+  @impl true
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> set_x_axis()
+     |> push_data_to_client()}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+    <div id="chart-container" phx-update="ignore">
+      <canvas id="chart" phx-hook="LotkaVolterraChart" width="400" height="200"></canvas>
+    </div>
+    """
+  end
+
+  defp set_x_axis(%{assigns: %{x_axis: x}} = socket), do: assign(socket, x_axis: x + 1)
+  defp set_x_axis(socket), do: assign(socket, x_axis: 0)
+
+  defp push_data_to_client(socket) do
+    socket
+    |> push_event("update-chart", %{
+      x_axis: socket.assigns.x_axis,
+      vegetation: socket.assigns.vegetation.size
+    })
+  end
+end
