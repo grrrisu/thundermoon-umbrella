4c54e2cf193815eaf15231d602bd398ecdb06ea8
Alessandro Di Maria <adm@m42.ch>
2020-08-15 21:15:06 +0200

    apply_params in form_data

--- apps/thundermoon/test/thundermoon/lotka_volterra/form_data_test.exs
+++ apps/thundermoon/test/thundermoon/lotka_volterra/form_data_test.exs
@@ -6,20 +6,38 @@ defmodule Thundermoon.LotkaVolterra.FormDataTest do
   alias Thundermoon.LotkaVolterra.FormData
   alias LotkaVolterra.Vegetation
 
-  test "set default values" do
-    changeset = FormData.changeset(%Vegetation{})
-    assert 650 == Changeset.get_field(changeset, :size)
+  describe "changeset" do
+    test "set default values" do
+      changeset = FormData.changeset(%Vegetation{})
+      assert 650 == Changeset.get_field(changeset, :size)
+    end
+
+    test "overwrite values" do
+      valid_params = %{capacity: 5000, birth_rate: 0.5, death_rate: 0.1, size: 800}
+      changeset = FormData.changeset(%Vegetation{size: 650}, valid_params)
+      assert 800 == Changeset.get_field(changeset, :size)
+    end
+
+    test "prevent invalid values" do
+      changeset = FormData.changeset(%Vegetation{size: 650}, %{size: -50})
+      refute changeset.valid?
+      refute changeset.errors[:size] |> is_nil()
+    end
   end
 
-  test "overwrite values" do
-    valid_params = %{capacity: 5000, birth_rate: 0.5, death_rate: 0.1, size: 800}
-    changeset = FormData.changeset(%Vegetation{size: 650}, valid_params)
-    assert 800 == Changeset.get_field(changeset, :size)
-  end
+  describe "apply params" do
+    test "valid" do
+      assert {:ok, %Vegetation{} = vegetation} =
+               FormData.apply_params(%Vegetation{}, %{size: 800})
 
-  test "prevent invalid values" do
-    changeset = FormData.changeset(%Vegetation{size: 650}, %{size: -50})
-    refute changeset.valid?()
-    refute changeset.errors[:size] |> is_nil()
+      assert vegetation.size == 800
+    end
+
+    test "invalid" do
+      assert {:error, %Changeset{} = changeset} =
+               FormData.apply_params(%Vegetation{}, %{size: -50})
+
+      assert -50 == Changeset.get_field(changeset, :size)
+    end
   end
 end
