660134579e4a657bf80226376f2055b181b98ffa
Alessandro Di Maria <adm@m42.ch>
2020-08-15 21:16:10 +0200

    text input component for milligramm forms

--- apps/thundermoon_web/lib/thundermoon_web/live/live_helpers.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/live_helpers.ex
@@ -1,2 +1,11 @@
 defmodule ThundermoonWeb.LiveHelpers do
+  import Phoenix.LiveView.Helpers
+
+  def live_text_input(socket, form, field, opts \\ []) do
+    live_component(socket, ThundermoonWeb.Component.Form.TextInput,
+      form: form,
+      field: field,
+      opts: opts
+    )
+  end
 end
