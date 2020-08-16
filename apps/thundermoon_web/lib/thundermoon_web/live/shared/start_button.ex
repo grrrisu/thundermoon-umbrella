1baf87303c8beeaed2cc92c72bb0818da8dd4bcd
Alessandro Di Maria <adm@m42.ch>
2020-08-16 12:24:36 +0200

    move handle_event of start button to component itself

--- apps/thundermoon_web/lib/thundermoon_web/live/shared/start_button.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/shared/start_button.ex
@@ -4,16 +4,29 @@ defmodule ThundermoonWeb.Component.StartButton do
   def update(assigns, socket) do
     {:ok,
      socket
+     |> assign(assigns)
      |> assign(icon: set_icon(assigns.started))
      |> assign(label: set_label(assigns.started))}
   end
 
   def render(assigns) do
     ~L"""
-    <a href="#" id="start-button" phx-click="toggle-sim-start" phx-value-action="<%= @label %>" class="button button-icon">
+    <a href="#" id="start-button" phx-click="toggle-sim-start" phx-value-action="<%= @label %>" phx-target="<%= @myself %>" class="button button-icon">
       <i class="la la-2x <%= @icon %>"></i><%= @label %>
     </a>
     """
+  end
+
+  @impl true
+  def handle_event("toggle-sim-start", %{"action" => "start"}, socket) do
+    send(self(), :start)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("toggle-sim-start", %{"action" => "stop"}, socket) do
+    send(self(), :stop)
+    {:noreply, socket}
   end
 
   defp set_icon(false), do: "la-play"
