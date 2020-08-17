0eb5a5bad01e52396383ed49ba03d312cadfe474
Alessandro Di Maria <adm@m42.ch>
2020-08-17 10:44:08 +0200

    integration test recreate simulation

--- apps/thundermoon_web/test/cypress/integration/lotka_volterra_spec.js
+++ apps/thundermoon_web/test/cypress/integration/lotka_volterra_spec.js
@@ -10,16 +10,31 @@ describe("lotka volterra page", function() {
 
     beforeEach(function() {
       cy.login(123);
-      cy.visit("/lotka-volterra");
+      cy.visit("/lotka-volterra/new");
+      cy.get("h1").contains("Lotka Volterra");
     });
 
     it("I can start a simulation", function() {
-      cy.get("h1").contains("Lotka Volterra");
+      cy.get("input#vegetation_capacity").clear().type("5000");
+      cy.get("input#vegetation_birth_rate").clear().type("0.5");
+      cy.get("input#vegetation_death_rate").clear().type("0.1");
+      cy.get("input#vegetation_size").clear().type("150");
+      cy.get("form").submit();
+
       cy.get("#chart");
-      cy.contains("create").click();
       cy.contains("start").click();
       cy.contains("stop").click();
     });
+
+    it("I can recreate a simulation", function(){
+      cy.get("input#vegetation_capacity").clear().type("5000");
+      cy.get("form").submit();
+      cy.get("#chart");
+      cy.contains("recreate").click();
+      cy.get("input#vegetation_size").clear().type("150");
+      cy.get("form").submit();
+      cy.get("#chart");
+    })
 
   });
 
