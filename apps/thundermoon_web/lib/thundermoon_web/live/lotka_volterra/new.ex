32520cd1e15bf0489371f04ac262bba6210d05cf
Alessandro Di Maria <adm@m42.ch>
2020-09-18 15:03:59 +0200

    handle event add herbivore

--- apps/thundermoon_web/lib/thundermoon_web/live/lotka_volterra/new.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/lotka_volterra/new.ex
@@ -6,11 +6,28 @@ defmodule ThundermoonWeb.LotkaVolterraLive.New do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok,
-     socket
-     |> assign(changeset: empty_form())}
+    {:ok, socket}
   end
 
+  def handle_params(%{}, url, socket) do
+    {:noreply, assign(socket, changeset: empty_form())}
+  end
+
+  def handle_params(%{"model" => "herbivore"}, url, socket) do
+    {:noreply, assign(socket, changeset: empty_form())}
+  end
+
+  def handle_event("add_herbivore", _params, socket) do
+    case VegetationForm.apply_valid_changes(socket.assigns.changeset) do
+      {:ok, vegetation} ->
+        {:noreply,
+         socket
+         |> assign(vegetation: vegetation)
+         |> push_patch(to: Routes.lotka_volterra_new_path(socket, :new, model: "herbivore"))}
+
+      {:error, changeset} ->
+        {:noreply, validation_failed(socket, changeset, "creating vegetation failed")}
+    end
   end
 
   @impl true
@@ -24,8 +41,8 @@ defmodule ThundermoonWeb.LotkaVolterraLive.New do
   end
 
   @impl true
-  def handle_event("create", %{"vegetation" => params}, socket) do
-    case FormData.apply_params(%Vegetation{}, params) do
+  def handle_event("create", %{"vegetation" => params} = p, socket) do
+    case apply_params(params) do
       {:ok, vegetation} ->
         sim_id = create_sim(vegetation)
 
@@ -35,15 +52,23 @@ defmodule ThundermoonWeb.LotkaVolterraLive.New do
          |> push_redirect(to: Routes.lotka_volterra_index_path(socket, :index, sim_id: sim_id))}
 
       {:error, changeset} ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "creating vegetation failed")
-         |> assign(changeset: changeset)}
+        {:noreply, validation_failed(socket, changeset, "creating vegetation failed")}
     end
   end
 
-  defp create_sim(vegetation) do
-    {sim_id, _object} = LotkaVolterra.create({vegetation, %Herbivore{}}, ThundermoonWeb.PubSub)
+  defp validation_failed(socket, changeset, message) do
+    socket
+    |> put_flash(:error, message)
+    |> assign(changeset: changeset)
+  end
+
+  defp apply_params(params) do
+    VegetationForm.changeset(%Vegetation{}, params)
+    |> VegetationForm.apply_valid_changes()
+  end
+
+  defp create_sim(vegetation, herbivore \\ nil) do
+    {sim_id, _object} = LotkaVolterra.create({vegetation, herbivore}, ThundermoonWeb.PubSub)
     sim_id
   end
 
