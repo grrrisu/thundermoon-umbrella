f3719ad407c3bcecee264868b8cf70eef2b68176
Alessandro Di Maria <adm@m42.ch>
2020-09-18 17:15:20 +0200

    form components for vegetation and herbivore

---
+++ apps/thundermoon/lib/thundermoon/lotka_volterra/herbivore_form.ex
@@ -0,0 +1,44 @@
+defmodule Thundermoon.LotkaVolterra.HerbivoreForm do
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  alias LotkaVolterra.Herbivore
+
+  @types %{
+    birth_rate: :float,
+    death_rate: :float,
+    needed_food: :integer,
+    starving_rate: :float,
+    graze_rate: :float,
+    size: :integer
+  }
+
+  def changeset(%Herbivore{} = model, params \\ %{}) do
+    {model, @types}
+    |> cast(params, [:birth_rate, :death_rate, :needed_food, :starving_rate, :graze_rate, :size])
+    |> validate_required(:size)
+    |> validate_number(:birth_rate, greater_than: 0)
+    |> validate_number(:death_rate, greater_than: 0)
+    |> validate_number(:needed_food, greater_than: 0)
+    |> validate_number(:starving_rate, greater_than: 0)
+    |> validate_number(:graze_rate, greater_than: 0)
+    |> validate_number(:size, greater_than: 0)
+    |> validate_greater_than(:birth_rate, :death_rate)
+  end
+
+  def validate_greater_than(changeset, field, other, opts \\ []) do
+    validate_change(changeset, field, fn _, value ->
+      case value >= Changeset.get_field(changeset, other) do
+        true -> []
+        false -> [{field, opts[:message] || "must be greater than #{other}"}]
+      end
+    end)
+  end
+
+  def apply_valid_changes(%Changeset{valid?: false} = changeset), do: {:error, changeset}
+
+  def apply_valid_changes(%Changeset{valid?: true} = changeset) do
+    {:ok, Changeset.apply_changes(changeset)}
+  end
+end
