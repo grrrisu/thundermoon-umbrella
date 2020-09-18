8d510cf72f203310644afbfbe8f86f0e5d00b49d
Alessandro Di Maria <adm@m42.ch>
2020-09-18 14:51:00 +0200

    rename form_data to vegetation_form as we will have other forms as well

--- apps/thundermoon/lib/thundermoon/lotka_volterra/vegetation_form.ex
+++ apps/thundermoon/lib/thundermoon/lotka_volterra/vegetation_form.ex
@@ -1,4 +1,4 @@
-defmodule Thundermoon.LotkaVolterra.FormData do
+defmodule Thundermoon.LotkaVolterra.VegetationForm do
   import Ecto.Changeset
 
   alias Ecto.Changeset
@@ -20,11 +20,11 @@ defmodule Thundermoon.LotkaVolterra.FormData do
     |> validate_number(:birth_rate, greater_than: 0)
     |> validate_number(:death_rate, greater_than: 0)
     |> validate_number(:size, greater_than: 0)
-    |> vaidate_greater_than(:capacity, :size)
-    |> vaidate_greater_than(:birth_rate, :death_rate)
+    |> validate_greater_than(:capacity, :size)
+    |> validate_greater_than(:birth_rate, :death_rate)
   end
 
-  def vaidate_greater_than(changeset, field, other, opts \\ []) do
+  def validate_greater_than(changeset, field, other, opts \\ []) do
     validate_change(changeset, field, fn _, value ->
       case value >= Changeset.get_field(changeset, other) do
         true -> []
@@ -33,15 +33,9 @@ defmodule Thundermoon.LotkaVolterra.FormData do
     end)
   end
 
-  def apply_params(model, params) do
-    model
-    |> changeset(params)
-    |> apply_valid_changes()
-  end
+  def apply_valid_changes(%Changeset{valid?: false} = changeset), do: {:error, changeset}
 
-  defp apply_valid_changes(%Changeset{valid?: false} = changeset), do: {:error, changeset}
-
-  defp apply_valid_changes(%Changeset{valid?: true} = changeset) do
+  def apply_valid_changes(%Changeset{valid?: true} = changeset) do
     {:ok, Changeset.apply_changes(changeset)}
   end
 end
--- apps/thundermoon/test/thundermoon/lotka_volterra/vegetation_form_test.exs
+++ apps/thundermoon/test/thundermoon/lotka_volterra/vegetation_form_test.exs
@@ -1,48 +1,45 @@
-defmodule Thundermoon.LotkaVolterra.FormDataTest do
+defmodule Thundermoon.LotkaVolterra.VegetationFormTest do
   use ExUnit.Case, async: true
 
   alias Ecto.Changeset
 
-  alias Thundermoon.LotkaVolterra.FormData
+  alias Thundermoon.LotkaVolterra.VegetationForm
   alias LotkaVolterra.Vegetation
 
   describe "changeset" do
     test "set default values" do
-      changeset = FormData.changeset(%Vegetation{})
+      changeset = VegetationForm.changeset(%Vegetation{})
       assert 650 == Changeset.get_field(changeset, :size)
     end
 
     test "overwrite values" do
       valid_params = %{capacity: 5000, birth_rate: 0.5, death_rate: 0.1, size: 800}
-      changeset = FormData.changeset(%Vegetation{size: 650}, valid_params)
+      changeset = VegetationForm.changeset(%Vegetation{size: 650}, valid_params)
       assert 800 == Changeset.get_field(changeset, :size)
     end
 
     test "prevent invalid values" do
-      changeset = FormData.changeset(%Vegetation{size: 650}, %{size: -50})
+      changeset = VegetationForm.changeset(%Vegetation{size: 650}, %{size: -50})
       refute changeset.valid?
       refute changeset.errors[:size] |> is_nil()
     end
 
     test "validate size smaller than capacity" do
-      changeset = FormData.changeset(%Vegetation{}, %{capacity: 50, size: 500})
+      changeset = VegetationForm.changeset(%Vegetation{}, %{capacity: 50, size: 500})
       refute changeset.valid?
       refute changeset.errors[:capacity] |> is_nil()
     end
   end
 
-  describe "apply params" do
+  describe "apply valid changes" do
     test "valid" do
-      assert {:ok, %Vegetation{} = vegetation} =
-               FormData.apply_params(%Vegetation{}, %{size: 800})
-
-      assert vegetation.size == 800
+      changeset = VegetationForm.changeset(%Vegetation{}, %{size: 800})
+      assert {:ok, %Vegetation{} = vegetation} = VegetationForm.apply_valid_changes(changeset)
     end
 
     test "invalid" do
-      assert {:error, %Changeset{} = changeset} =
-               FormData.apply_params(%Vegetation{}, %{size: -50})
-
+      changeset = VegetationForm.changeset(%Vegetation{}, %{size: -50})
+      assert {:error, %Changeset{} = changeset} = VegetationForm.apply_valid_changes(changeset)
       assert -50 == Changeset.get_field(changeset, :size)
     end
   end
