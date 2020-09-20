2cc68592c269c7b35bef41a9455c7cc6a508889b
Alessandro Di Maria <adm@m42.ch>
2020-09-20 13:55:46 +0200

    add cypress tests for adding herbivores to the simulation

--- apps/thundermoon_web/test/cypress/integration/lotka_volterra_spec.js
+++ apps/thundermoon_web/test/cypress/integration/lotka_volterra_spec.js
@@ -26,6 +26,23 @@ describe("lotka volterra page", function() {
       cy.contains("stop").click();
     });
 
+    it("I can add a herbivore to the simulation", function(){
+      cy.get("h3").contains("Vegetation");
+      cy.get("a").contains("Add Herbivore").click()
+      cy.get("h3").contains("Herbivore");
+      cy.get("input#herbivore_birth_rate").clear().type("0.5");
+      cy.get("input#herbivore_death_rate").clear().type("0.1");
+      cy.get("input#herbivore_needed_food").clear().type("2");
+      cy.get("input#herbivore_starving_rate").clear().type("0.2");
+      cy.get("input#herbivore_graze_rate").clear().type("0.05");
+      cy.get("input#herbivore_size").clear().type("20");
+      cy.get("form").submit();
+
+      cy.get("#chart");
+      cy.contains("start").click();
+      cy.contains("stop").click();
+    });
+
     it("I can recreate a simulation", function(){
       cy.get("input#vegetation_capacity").clear().type("5000");
       cy.get("form").submit();
