2783b0eadc559ee34cca1b410279541691453842
Alessandro Di Maria <adm@m42.ch>
2020-10-06 18:34:04 +0200

    form entity component

--- apps/thundermoon_web/lib/thundermoon_web/live/lotka_volterra_live/new_herbivore.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/lotka_volterra_live/new_herbivore.ex
@@ -1,3 +1,6 @@
 defmodule ThundermoonWeb.LotkaVolterraLive.NewHerbivore do
-  use ThundermoonWeb, :live_component
+  use ThundermoonWeb.LotkaVolterra.Entity,
+    params_name: "herbivore",
+    model: LotkaVolterra.Herbivore,
+    form_data: Thundermoon.LotkaVolterra.HerbivoreForm
 end
