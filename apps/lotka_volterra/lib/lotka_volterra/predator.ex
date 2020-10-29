d7533b6bd547eec6681a29d36684709cf32957b1
Alessandro Di Maria <adm@m42.ch>
2020-10-29 14:48:51 +0100

    redirect to initial vegi form, if we loose state because of reloads

--- apps/lotka_volterra/lib/lotka_volterra/predator.ex
+++ apps/lotka_volterra/lib/lotka_volterra/predator.ex
@@ -1,8 +1,8 @@
 defmodule LotkaVolterra.Predator do
   defstruct birth_rate: 0.5,
             death_rate: 0.01,
-            needed_food: 5,
-            starving_rate: 0.4,
-            graze_rate: 0.05,
-            size: 50
+            needed_food: 3,
+            starving_rate: 0.3,
+            graze_rate: 0.1,
+            size: 10
 end
