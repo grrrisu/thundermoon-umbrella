e4994d518a17faf0f8583e1d737f25949f2d3a61
Alessandro Di Maria <adm@m42.ch>
2021-01-04 15:22:47 +0100

    bump version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Thundermoon.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.4.0",
+      version: "0.5.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [
