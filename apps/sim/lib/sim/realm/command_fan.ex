3241e00be039ac57ef5036d2302c3eb55270e7ee
Alessandro Di Maria <adm@m42.ch>
2021-01-29 22:26:55 +0100

    handle answers from finished command handler

--- apps/sim/lib/sim/realm/command_fan.ex
+++ apps/sim/lib/sim/realm/command_fan.ex
@@ -8,21 +8,25 @@ defmodule Sim.Realm.CommandFan do
 
   require Logger
 
-  @type command :: {atom, atom, keyword}
+  @type context :: atom
+  @type cmd :: atom
+  @type command :: {context, cmd, keyword}
 
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts[:services], name: opts[:name])
+    GenServer.start_link(__MODULE__, opts, name: opts[:name])
   end
 
   @impl true
-  def init(services) do
-    {:ok, init_services(services)}
+  def init(opts) do
+    {:ok,
+     services: init_services(opts[:services]),
+     task_supervisor_module: opts[:task_supervisor_module]}
   end
 
   def init_services(services) do
     services
     |> Enum.reduce(%{}, fn {service, module}, acc ->
-      value = %{module: module, running_command: nil, queue: :queue.new()}
+      value = %{module: module, running_command: nil, running_ref: nil, queue: :queue.new()}
       Map.put_new(acc, service, value)
     end)
   end
@@ -32,29 +36,107 @@ defmodule Sim.Realm.CommandFan do
     GenServer.call(server, {:dispatch, command})
   end
 
+  def handle_call({:dispatch, {context, cmd}}, from, state) do
+    handle_call({:dispatch, {context, cmd, []}}, from, state)
+  end
+
   @impl true
-  def handle_call({:dispatch, {context, _, _}} = command, _from, state) do
-    case Map.fetch(state, context) do
-      :error -> {:reply, {:error, "no service found for #{context}"}, state}
-      service -> {:reply, :ok, %{state | context => queue_or_execute(command, service)}}
+  def handle_call({:dispatch, {context, _, _} = command}, _from, state) do
+    case Map.fetch(state.services, context) do
+      :error ->
+        {:reply, {:error, "no service found for #{context}"}, state}
+
+      {:ok, service} ->
+        {:reply, :ok, assign_service(state, context, queue_or_execute(command, service, state))}
     end
   end
 
-  @spec queue_or_execute(command, map) :: map
-  def queue_or_execute(command, %{module: module, running_task: nil} = service_state) do
+  # The task completed successfully
+  @impl true
+  def handle_info({ref, events}, state) do
+    case find_ended_task(ref, state.services) do
+      nil ->
+        noreply_unknown_ref(ref, "received answer #{inspect(events)}", state)
+
+      {context, service} ->
+        # We don't care about the DOWN message now, so let's demonitor and flush it
+        Process.demonitor(ref, [:flush])
+
+        Logger.debug(
+          "task executing command #{inspect(service.running_command)} finished with result #{
+            inspect(events)
+          }"
+        )
+
+        {:noreply, assign_service(state, context, handle_answer(events, service, state))}
+    end
+  end
+
+  # The task failed
+  @impl true
+  def handle_info({:DOWN, ref, :process, _pid, reason}, state) do
+    case find_ended_task(ref, state.services) do
+      nil ->
+        noreply_unknown_ref(ref, "received DOWN", state)
+
+      {context, service} ->
+        Logger.debug(
+          "task executing command #{inspect(service.running_command)} failed with reason #{
+            inspect(reason)
+          }"
+        )
+
+        # add_command({:sim_stop})
+        events = [{:command_failed, command: service.running_command, reason: reason}]
+
+        {:noreply, assign_service(state, context, handle_answer(events, service, state))}
+    end
+  end
+
+  @spec queue_or_execute(command, map, map) :: map
+  def queue_or_execute(command, %{module: module, running_command: nil} = service_state, state) do
     task =
       Task.Supervisor.async_nolink(
-        __MODULE__,
+        state.task_supervisor_module,
         fn ->
           module.execute(command)
         end
       )
 
-    %{service_state | running_task: task.ref}
+    %{service_state | running_command: command, running_ref: task.ref}
   end
 
-  @spec queue_or_execute(command, map) :: map
-  def queue_or_execute(command, %{queue: queue} = service_state) do
+  @spec queue_or_execute(command, map, map) :: map
+  def queue_or_execute(command, %{queue: queue} = service_state, _state) do
     %{service_state | queue: :queue.in(command, queue)}
+  end
+
+  def store_events(events) do
+    Logger.info(inspect(events))
+  end
+
+  defp find_ended_task(ref, services) do
+    Enum.find(services, fn {_context, service} ->
+      service.running_ref == ref
+    end)
+  end
+
+  defp handle_answer(events, service, state) do
+    store_events(events)
+    service = %{service | running_ref: nil, running_command: nil}
+
+    case :queue.out(service.queue) do
+      {{:value, command}, queue} -> queue_or_execute(command, %{service | queue: queue}, state)
+      {:empty, _queue} -> service
+    end
+  end
+
+  defp noreply_unknown_ref(ref, message, state) do
+    Logger.warn("#{message} from unkown ref #{inspect(ref)}")
+    {:noreply, state}
+  end
+
+  defp assign_service(state, context, service) do
+    put_in(state, [:services, context], service)
   end
 end
--- apps/sim/test/sim/realm/command_fan_test.exs
+++ apps/sim/test/sim/realm/command_fan_test.exs
@@ -3,26 +3,98 @@ defmodule Sim.Realm.CommandFanTest do
 
   alias Sim.Realm.CommandFan
 
-  setup do
-    state = %{
-      sim: %{
-        module: SimService,
-        running_command: nil,
-        queue: {[], []}
-      },
-      movement: %{
-        module: MovementService,
-        running_command: nil,
-        queue: {[], []}
+  describe "init" do
+    setup do
+      state = %{
+        sim: %{
+          module: SimService,
+          running_command: nil,
+          running_ref: nil,
+          queue: :queue.new()
+        },
+        action: %{
+          module: ActionService,
+          running_command: nil,
+          running_ref: nil,
+          queue: :queue.new()
+        }
       }
-    }
 
-    %{state: state}
+      %{state: state}
+    end
+
+    test "init services", %{state: state} do
+      services = %{sim: SimService, action: ActionService}
+      initialized_state = CommandFan.init_services(services)
+      assert initialized_state == state
+    end
   end
 
-  test "init services", %{state: state} do
-    services = %{sim: SimService, movement: MovementService}
-    initialized_state = CommandFan.init_services(services)
-    assert initialized_state == state
+  describe "dispatch command" do
+    setup do
+      {:ok, task_supervisor} = start_supervised(Task.Supervisor)
+      {:ok, task_pid} = Task.start(fn -> :nothing end)
+      task_ref = Process.monitor(task_pid)
+
+      state = %{
+        services: %{
+          sim: %{
+            module: SimService,
+            running_command: nil,
+            running_ref: nil,
+            queue: :queue.new()
+          },
+          action: %{
+            module: ActionService,
+            running_command: {:action, :move, x: 7, y: 3},
+            running_ref: "a_ref",
+            queue: :queue.new()
+          },
+          admin: %{
+            module: AdminService,
+            running_command: {:admin, :create, size: 25},
+            running_ref: task_ref,
+            queue: :queue.from_list([{:admin, :start_sim, []}])
+          }
+        },
+        task_supervisor_module: task_supervisor
+      }
+
+      %{state: state, task_ref: task_ref}
+    end
+
+    test "queue if another command of the same service is already running", %{state: state} do
+      command = {:action, :move, x: 5, y: 5}
+      assert {:reply, :ok, state} = CommandFan.handle_call({:dispatch, command}, "from", state)
+
+      assert :queue.to_list(state.services.action.queue) == [command]
+      assert state.services.action.running_command == {:action, :move, x: 7, y: 3}
+    end
+
+    test "execute command if for this services no other task is running", %{state: state} do
+      command = {:sim, :world}
+      assert {:reply, :ok, state} = CommandFan.handle_call({:dispatch, command}, "from", state)
+
+      assert :queue.len(state.services.sim.queue) == 0
+      assert is_reference(state.services.sim.running_ref)
+      assert state.services.sim.running_command == {:sim, :world, []}
+    end
+
+    test "execute next command after task has finished", %{state: state, task_ref: task_ref} do
+      {:noreply, state} = CommandFan.handle_info({task_ref, ["an_event"]}, state)
+
+      assert state.services.admin.running_command == {:admin, :start_sim, []}
+      assert is_reference(state.services.admin.running_ref)
+      assert :queue.len(state.services.admin.queue) == 0
+    end
+
+    test "execute next command even after task has failed", %{state: state, task_ref: task_ref} do
+      {:noreply, state} =
+        CommandFan.handle_info({:DOWN, task_ref, :process, "pid", :error}, state)
+
+      assert state.services.admin.running_command == {:admin, :start_sim, []}
+      assert is_reference(state.services.admin.running_ref)
+      assert :queue.len(state.services.admin.queue) == 0
+    end
   end
 end
