b3ee2e507ea14ff09c0c0e26724128df771a4997
Alessandro Di Maria <adm@m42.ch>
2021-04-18 12:50:30 +0200

    extract message to its own component

---
+++ apps/thundermoon_web/lib/thundermoon_web/live/chat_live/message.ex
@@ -0,0 +1,44 @@
+defmodule ThundermoonWeb.ChatLive.Message do
+  use ThundermoonWeb, :live_component
+
+  @impl true
+  def update(assigns, socket) do
+    {:ok,
+      socket
+      |> assign(assigns)
+      |> assign_is_current_user
+      |> assign_message_color
+    }
+  end
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+    <div class="flex">
+      <%= if @is_current_user do %>
+        <div class="w-3/12"></div>
+      <% end %>
+      <div class="<%= @message_color %> rounded-md px-4 py-2 mb-2 w-9/12">
+        <div class="font-bold"><%= @message.user %></div>
+        <div class="font-light"><%= @message.text %></div>
+      </div>
+      <%= unless @is_current_user do %>
+        <div class="w-3/12"></div>
+      <% end %>
+    </div>
+    """
+  end
+
+  defp assign_is_current_user(%{assigns: %{current_user: user, message: message}} = socket) do
+    assign(socket, :is_current_user, user.id == message.user_id)
+  end
+
+  defp assign_message_color(socket) do
+    color = case socket.assigns.is_current_user do
+      true -> "bg-indigo-500"
+      false -> "bg-gray-500"
+    end
+    assign(socket, :message_color, color)
+  end
+
+end
