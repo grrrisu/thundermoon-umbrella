b53ba91240e4613bbb8803b42ac1d91bbe3d28d0
Alessandro Di Maria <adm@m42.ch>
2021-04-21 09:11:45 +0200

    check if live_view is connected before each spec

--- apps/thundermoon_web/test/cypress/integration/counter_spec.js
+++ apps/thundermoon_web/test/cypress/integration/counter_spec.js
@@ -1,18 +1,18 @@
 /* global cy */
 
-describe("counter page", function() {
-
-  beforeEach(function() {
+describe("counter page", function () {
+  beforeEach(function () {
     cy.login(456);
     cy.visit("/counter");
+    cy.get(".phx-connected");
     cy.contains("reset").click();
   });
 
-  afterEach(function() {
+  afterEach(function () {
     cy.logout();
   });
 
-  it("inc and dec all digits", function() {
+  it("inc and dec all digits", function () {
     cy.get("#digit-1").should("have.text", "0");
     cy.get("#digit-1-inc").click();
     cy.get("#digit-1").should("have.text", "1");
@@ -32,11 +32,11 @@ describe("counter page", function() {
     cy.get("#digit-100").should("have.text", "0");
   });
 
-  it("inc next digit", function() {
+  it("inc next digit", function () {
     cy.get("#digit-10").should("have.text", "0");
     cy.get("#digit-100").should("have.text", "0");
 
-    Array(...Array(9)).map((_n,_i) => {
+    Array(...Array(9)).map((_n, _i) => {
       cy.get("#digit-10-inc").click();
     });
     cy.get("#digit-10").should("have.text", "9");
@@ -46,7 +46,7 @@ describe("counter page", function() {
     cy.get("#digit-100").should("have.text", "1");
   });
 
-  it("dec same digit should reset counter", function() {
+  it("dec same digit should reset counter", function () {
     cy.get("#digit-1-inc").click();
     cy.get("#digit-1").should("have.text", "1");
     cy.get("#digit-10-inc").click();
@@ -60,7 +60,7 @@ describe("counter page", function() {
     cy.get("#digit-100").should("have.text", "0");
   });
 
-  it("dec next digit", function() {
+  it("dec next digit", function () {
     cy.get("#digit-100-inc").click();
     cy.get("#digit-100").should("have.text", "1");
 
@@ -71,7 +71,7 @@ describe("counter page", function() {
     cy.get("#digit-100").should("have.text", "0");
   });
 
-  it("reset counter", function() {
+  it("reset counter", function () {
     cy.get("#digit-1-inc").click();
     cy.get("#digit-1").should("have.text", "1");
     cy.get("#digit-10-inc").click();
@@ -86,7 +86,7 @@ describe("counter page", function() {
     cy.get("#digit-100").should("have.text", "0");
   });
 
-  it("start counter sim", function() {
+  it("start counter sim", function () {
     cy.get("#digit-1").should("have.text", "0");
     cy.contains("start").click();
     cy.get("#digit-1").should("have.text", "1");
