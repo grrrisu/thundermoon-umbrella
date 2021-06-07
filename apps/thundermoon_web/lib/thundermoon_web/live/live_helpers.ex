908512d886a142d14fa9ae6ae2a836fe960a609d
Alessandro Di Maria <adm@m42.ch>
2021-06-07 17:49:31 +0200

    remove obselete socket param for live_component

--- apps/thundermoon_web/lib/thundermoon_web/live/live_helpers.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/live_helpers.ex
@@ -1,15 +1,15 @@
 defmodule ThundermoonWeb.LiveHelpers do
   import Phoenix.LiveView.Helpers
 
-  def live_text_input(socket, form, field, opts \\ []) do
-    live_component(socket, ThundermoonWeb.Component.Form.TextInput,
+  def live_text_input(form, field, opts \\ []) do
+    live_component(ThundermoonWeb.Component.Form.TextInput,
       form: form,
       field: field,
       opts: Keyword.merge([class: "input text-input mb-3"], opts)
     )
   end
 
-  def start_button(socket, id, started) do
-    live_component(socket, ThundermoonWeb.Component.StartButton, id: id, started: started)
+  def start_button(id, started) do
+    live_component(ThundermoonWeb.Component.StartButton, id: id, started: started)
   end
 end
