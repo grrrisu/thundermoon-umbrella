05c6e041298adf90e87da5b6120a28bb76520865
Alessandro Di Maria <adm@m42.ch>
2021-06-21 17:48:08 +0200

    cypress integration test

--- apps/thundermoon_web/test/cypress/integration/page_spec.js
+++ apps/thundermoon_web/test/cypress/integration/page_spec.js
@@ -1,23 +1,33 @@
 /* global cy */
 
-describe("root page", function() {
-  it("should see the title", function() {
-    cy.visit("/");
+describe("root page", function () {
+  beforeEach(function () {
+    cy.login("/");
+  });
+
+  it("should see the title", function () {
     cy.contains("Thundermoon");
+  });
+
+  it("should show preview images", function () {
+    cy.get('img[alt="chat"]').should("be.visible");
+    cy.get('img[alt="counter"]').should("not.be.visible");
+    cy.get("a").contains("counter").click();
+    cy.get('img[alt="chat"]').should("not.be.visible");
+    cy.get('img[alt="counter"]').should("be.visible");
   });
 });
 
-describe("dashboard page", function() {
-
-  beforeEach(function() {
+describe("dashboard page", function () {
+  beforeEach(function () {
     cy.login(123);
   });
 
-  afterEach(function() {
+  afterEach(function () {
     cy.logout();
   });
 
-  it("should see the title", function() {
+  it("should see the title", function () {
     cy.visit("/dashboard");
     cy.contains("Welcome crumb");
   });
