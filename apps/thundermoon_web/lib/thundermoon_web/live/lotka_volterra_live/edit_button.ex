cb91f8371d08489001afd1126b3d73a1aea91676
Alessandro Di Maria <adm@m42.ch>
2021-07-03 16:55:44 +0200

    edit button component

---
+++ apps/thundermoon_web/lib/thundermoon_web/live/lotka_volterra_live/edit_button.ex
@@ -0,0 +1,21 @@
+defmodule ThundermoonWeb.LotkaVolterraLive.EditButton do
+  use ThundermoonWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+    <div x-data="{ showForm: false }" x-cloak x-transition class="inline relative">
+      <a @click.prevent="showForm = !showForm" class="btn btn-outline">
+        <i class="las la-plus-circle"></i>
+        <%= @button_label %>
+      </a>
+      <div x-show="showForm" class="absolute left-0 bottom-7">
+        <div class="border bg-gray-800 border-gray-600 rounded-md p-4">
+          <%= render_block(@inner_block) %>
+        </div>
+        <i class="las la-angle-down"></i>
+      </div>
+    </div>
+    """
+  end
+
+end
