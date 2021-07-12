e85d7854203e0d314d983f35fb1daa6ef12609e1
Alessandro Di Maria <adm@m42.ch>
2021-07-12 13:12:17 +0200

    component flash alerts

--- apps/thundermoon_web/lib/thundermoon_web/live/live_helpers.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/live_helpers.ex
@@ -1,6 +1,10 @@
 defmodule ThundermoonWeb.LiveHelpers do
   import Phoenix.LiveView.Helpers
 
+  def live_flash_alerts(assigns) do
+    live_component(ThundermoonWeb.Component.FlashAlert, assigns)
+  end
+
   def live_text_input(form, field, opts \\ []) do
     live_component(ThundermoonWeb.Component.Form.TextInput,
       form: form,
