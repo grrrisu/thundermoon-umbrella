e31a3c051cc72628928fbd8c64d3dee91dddd529
Alessandro Di Maria <adm@m42.ch>
2021-07-12 09:18:46 +0200

    validate greater also other if smaller

--- apps/thundermoon/lib/thundermoon/form_data.ex
+++ apps/thundermoon/lib/thundermoon/form_data.ex
@@ -4,10 +4,25 @@ defmodule Thundermoon.FormData do
       import Ecto.Changeset
 
       def validate_greater_than(changeset, field, other, opts \\ []) do
+        changeset
+        |> validate_changed_greater_than(field, other, opts)
+        |> validate_changed_smaller_than(other, field, opts)
+      end
+
+      defp validate_changed_greater_than(changeset, field, other, opts \\ []) do
         validate_change(changeset, field, fn _, value ->
           case value >= get_field(changeset, other) do
             true -> []
             false -> [{field, opts[:message] || "must be greater than #{other}"}]
+          end
+        end)
+      end
+
+      defp validate_changed_smaller_than(changeset, field, other, opts \\ []) do
+        validate_change(changeset, field, fn _, value ->
+          case value < get_field(changeset, other) do
+            true -> []
+            false -> [{field, opts[:message] || "must be smaller than #{other}"}]
           end
         end)
       end
