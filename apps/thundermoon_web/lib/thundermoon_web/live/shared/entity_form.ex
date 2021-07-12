0a95cbed944a84ead97971ce39d974c36b588c63
Alessandro Di Maria <adm@m42.ch>
2021-07-12 21:33:15 +0200

    clear flash in component on update or handle event

--- apps/thundermoon_web/lib/thundermoon_web/live/shared/entity_form.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/shared/entity_form.ex
@@ -19,6 +19,7 @@ defmodule ThundermoonWeb.Component.EntityForm do
         {:ok,
          socket
          |> assign(assigns)
+         |> clear_flash()
          |> assign(creating: not Map.has_key?(assigns, :data))
          |> assign(changeset: get_changeset(assigns))}
       end
@@ -30,17 +31,20 @@ defmodule ThundermoonWeb.Component.EntityForm do
           |> @form_data.changeset(params)
           |> Map.put(:action, :insert)
 
-        {:noreply, assign(socket, changeset: changeset)}
+        {:noreply, socket |> clear_flash |> assign(changeset: changeset)}
       end
 
       @impl true
       def handle_event("create", %{@params_name => params}, socket) do
+        socket = clear_flash(socket)
+
         case apply_params(params) do
           {:ok, entity} ->
             {:noreply, entity_submitted(entity, socket)}
 
           {:error, changeset} ->
-            {:noreply, validation_failed(socket, changeset, "creating #{@params_name} failed")}
+            {:noreply,
+             validation_failed(socket, changeset, "parameters for #{@params_name} are not valid")}
         end
       end
 
