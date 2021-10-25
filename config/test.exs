f7fb832af8b1914dfcb2fe5b847779585110a5f6
Alessandro Di Maria <adm@m42.ch>
2021-10-25 11:46:57 +0200

    adapt test config

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,8 @@ config :thundermoon, Thundermoon.Repo,
   database: "thundermoon_test",
   hostname: System.get_env("DB_HOST", "localhost"),
   show_sensitive_data_on_connection_error: true,
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
 
 if System.get_env("DB_USER") do
   config :thundermoon, Thundermoon.Repo,
@@ -16,9 +17,12 @@ end
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :thundermoon_web, ThundermoonWeb.Endpoint,
-  http: [port: 4002],
+  http: [ip: {127, 0, 0, 1}, port: 4002],
   url: [host: System.get_env("APP_HOST", "localhost")],
   server: true
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
