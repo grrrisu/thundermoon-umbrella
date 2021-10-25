765c42bbbff8ab2a4480d88d19dbf1f6354c7999
Alessandro Di Maria <adm@m42.ch>
2021-10-25 13:22:36 +0200

    adapt test sandbox support

--- apps/thundermoon_web/test/support/conn_case.ex
+++ apps/thundermoon_web/test/support/conn_case.ex
@@ -28,11 +28,8 @@ defmodule ThundermoonWeb.ConnCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Thundermoon.Repo)
-
-    unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(Thundermoon.Repo, {:shared, self()})
-    end
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Thundermoon.Repo, shared: not tags[:async])
+    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
