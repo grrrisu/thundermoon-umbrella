2c2d5e8eb51be7a5a20de09d53bd942e7a9d8691
Alessandro Di Maria <adm@m42.ch>
2021-10-25 13:53:26 +0200

    remove test and integration images from dockerhub after tests

---
+++ .semaphore/cleanup_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+ORGANIZATION="grrrocker"
+IMAGE="thundermoon-umbrella"
+
+login_data() {
+cat <<EOF
+{
+  "username": "$DOCKER_USERNAME",
+  "password": "$DOCKER_PASSWORD"
+}
+EOF
+}
+
+TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
+
+curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/test_${SEMAPHORE_GIT_SHA}/" \
+-X DELETE \
+-H "Authorization: JWT ${TOKEN}"
+
+curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/integration_${SEMAPHORE_GIT_SHA}/" \
+-X DELETE \
+-H "Authorization: JWT ${TOKEN}"
+
