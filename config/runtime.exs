bef861019c56c8043ed38bd1cb3a1c9fef4b7e67
Alessandro Di Maria <adm@m42.ch>
2021-10-25 11:40:29 +0200

    adapt prod/runtime configs

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,7 +10,12 @@ if config_env() == :prod do
   config :thundermoon_web, ThundermoonWeb.Endpoint,
     url: [host: "thundermoon.zero-x.net", port: 80],
     cache_static_manifest: "priv/static/cache_manifest.json",
-    http: [:inet6, port: String.to_integer(System.get_env("PORT", "4000"))],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: String.to_integer(System.get_env("PORT") || "4000")
+    ],
     secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
     live_view: [signing_salt: System.fetch_env!("SECRET_LIVE_VIEW_KEY")],
     check_origin: ["https://thundermoon.zero-x.net", "//localhost:4000"],
