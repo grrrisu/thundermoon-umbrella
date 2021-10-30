0e8db8c8b345d76bcae801f030bf362ef434e503
Alessandro Di Maria <adm@m42.ch>
2021-10-30 12:25:38 +0200

    refactor message component as stateless

--- apps/thundermoon_web/lib/thundermoon_web/live/chat_live/message.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/chat_live/message.ex
@@ -1,44 +1,30 @@
 defmodule ThundermoonWeb.ChatLive.Message do
-  use ThundermoonWeb, :live_component
+  use Phoenix.Component
 
-  @impl true
-  def update(assigns, socket) do
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign_is_current_user
-     |> assign_message_color}
-  end
-
-  @impl true
-  def render(assigns) do
+  def row(assigns) do
     ~H"""
     <div class="flex">
-      <%= if @is_current_user do %>
-        <div class="w-3/12"></div>
-      <% end %>
-      <div class={"#{@message_color} rounded-md px-4 py-2 mb-2 w-9/12"}>
+      <.indent for_current_user={true} current_user={@current_user} message={@message} />
+      <div class={"#{message_color(assigns)} rounded-md px-4 py-2 mb-2 w-9/12"}>
         <div class="font-bold"><%= @message.user %></div>
         <div class="font-light"><%= @message.text %></div>
       </div>
-      <%= unless @is_current_user do %>
-        <div class="w-3/12"></div>
-      <% end %>
+      <.indent for_current_user={false} current_user={@current_user} message={@message} />
     </div>
     """
   end
 
-  defp assign_is_current_user(%{assigns: %{current_user: user, message: message}} = socket) do
-    assign(socket, :is_current_user, user.id == message.user_id)
+  defp indent(assigns) do
+    ~H"""
+      <div class={is_current_user?(assigns) == @for_current_user && "w-3/12"}></div>
+    """
   end
 
-  defp assign_message_color(socket) do
-    color =
-      case socket.assigns.is_current_user do
-        true -> "bg-indigo-500"
-        false -> "bg-gray-500"
-      end
+  defp message_color(assigns) do
+    if is_current_user?(assigns), do: "bg-indigo-500", else: "bg-gray-500"
+  end
 
-    assign(socket, :message_color, color)
+  defp is_current_user?(%{current_user: current_user, message: message}) do
+    current_user.id == message.user_id
   end
 end
