fbdb367dafb116533fa4ba2b69288e5deb3b1145
Alessandro Di Maria <adm@m42.ch>
2021-10-31 16:25:26 +0100

    use Actions.reset_buttons

--- apps/thundermoon_web/lib/thundermoon_web/live/counter_live/action_buttons.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/counter_live/action_buttons.ex
@@ -12,18 +12,10 @@ defmodule ThundermoonWeb.CounterLive.ActionButtons do
         <Actions.box>
           <Actions.start_button started={@started} />
           <%= if can?(@current_user, :reset, Thundermoon.Counter) do %>
-            <.reset_button current_user={@current_user} />
+            <Actions.reset_button />
           <% end %>
         </Actions.box>
       </div>
-    """
-  end
-
-  def reset_button(assigns) do
-    ~H"""
-      <%= link to: "#", id: "reset-button", phx_click: "reset", class: "btn btn-warning" do %>
-        <i class="align-middle text-xl la la-reply"></i> reset
-      <% end %>
     """
   end
 end
