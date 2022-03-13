41712478e7eb2ee041a88e9ddee8fe8d0c9dcfe9
Alessandro Di Maria <adm@m42.ch>
2022-03-13 11:43:24 +0100

    allow multiple events to return from a command execution

--- apps/sim/test/sim/realm/domain_service_test.exs
+++ apps/sim/test/sim/realm/domain_service_test.exs
@@ -3,18 +3,18 @@ defmodule Sim.Realm.DoaminServiceTest do
 
   alias Sim.Realm.DomainService
 
-  def execute(:sim, []), do: [{:sim, :result}]
+  def execute(:sim, []), do: [{:sim, :result}, {:data, :updated}]
   def execute(:fail, arg: arg), do: {:error, "fail with #{arg}"}
   def execute(:crash, arg: arg), do: raise("crash #{arg}")
 
-  test "execute events" do
+  test "execute commands" do
     {:ok, worker_supervisor} = start_supervised(Task.Supervisor)
-    events = [{:sim, []}, {:fail, arg: "one"}, {:crash, arg: "command"}]
+    commands = [{:sim, []}, {:fail, arg: "one"}, {:crash, arg: "command"}]
 
     [res1, res2, res3] =
-      DomainService.execute(events, Sim.Realm.DoaminServiceTest, worker_supervisor)
+      DomainService.execute(commands, Sim.Realm.DoaminServiceTest, worker_supervisor)
 
-    assert {:ok, [{:sim, :result}]} = res1
+    assert {:ok, [{:sim, :result}, {:data, :updated}]} = res1
     assert {:ok, {:error, "fail with one"}} = res2
     assert {:exit, {%RuntimeError{message: "crash command"}, _stacktrace}} = res3
   end
@@ -22,6 +22,7 @@ defmodule Sim.Realm.DoaminServiceTest do
   test "filter" do
     results = [
       {:ok, [{:sim, :result}]},
+      {:ok, [{:sim, :result}, {:data, :updated}]},
       {:ok, {:error, "fail with one"}},
       {:exit, {%RuntimeError{message: "crash command"}, []}},
       {:exit, {:stopped, ["first"]}},
@@ -29,15 +30,17 @@ defmodule Sim.Realm.DoaminServiceTest do
       {:exit, "unknown error"}
     ]
 
-    [res1, res2, res3, res4, res5, res6] = DomainService.filter(results)
-    assert {:sim, :result} = res1
-    assert {:error, "fail with one"} = res2
-    assert {:error, "crash command"} = res3
-    assert {:error, "exited with stopped first"}
+    [res1, res2, res3, res4, res5, res6, res7] = DomainService.filter(results)
+    assert [{:sim, :result}] == res1
+    assert [{:sim, :result}, {:data, :updated}] == res2
+    assert {:error, "fail with one"} == res3
+    assert {:error, "crash command"} == res4
+    assert {:error, "exited with stopped \"first\""} == res5
 
     assert {:error,
-            "exited with undef {Meeple.Service.Admin, :execute, [:foobar]}, probaly an UndefinedFunctionError"}
+            "exited with undef \"{Meeple.Service.Admin, :execute, [:foobar]}\", probably an UndefinedFunctionError"} ==
+             res6
 
-    assert {:error, "unknown error: unknow error"}
+    assert {:error, "unknown error: \"unknown error\""} == res7
   end
 end
