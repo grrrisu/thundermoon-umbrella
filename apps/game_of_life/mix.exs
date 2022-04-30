589acb6c65fff74dd9dd627b287a431f13fb5168
Alessandro Di Maria <adm@m42.ch>
2022-04-30 09:41:15 +0200

    bump version to 0.9.0

--- apps/game_of_life/mix.exs
+++ apps/game_of_life/mix.exs
@@ -4,7 +4,7 @@ defmodule GameOfLife.MixProject do
   def project do
     [
       app: :game_of_life,
-      version: "0.5.0",
+      version: "0.6.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
