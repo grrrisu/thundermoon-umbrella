7f483cda97386d91034b029d32e80040603edbe3
Alessandro Di Maria <adm@m42.ch>
2022-10-24 11:01:24 +0200

    use import for action components

--- apps/thundermoon_web/lib/thundermoon_web/live/counter_live/digit.ex
+++ apps/thundermoon_web/lib/thundermoon_web/live/counter_live/digit.ex
@@ -2,7 +2,7 @@ defmodule ThundermoonWeb.CounterLive.Digit do
   use Phoenix.Component
   use Phoenix.HTML
 
-  def value(assigns) do
+  def digit(assigns) do
     ~H"""
       <div class="flex flex-col space-y-4">
         <%= link "+", to: "#", id: "#{@id}-inc", class: "btn text-2xl btn-primary", phx_click: "inc", phx_value_digit: @digit %>
