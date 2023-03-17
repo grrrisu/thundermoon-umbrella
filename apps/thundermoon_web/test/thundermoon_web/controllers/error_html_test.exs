28167001e2b5e9aeecbf8029eb539afd89d9d459
Alessandro Di Maria <adm@m42.ch>
2023-03-17 17:37:09 +0100

    make error html test viewless

--- apps/thundermoon_web/test/thundermoon_web/controllers/error_html_test.exs
+++ apps/thundermoon_web/test/thundermoon_web/controllers/error_html_test.exs
@@ -2,15 +2,15 @@ defmodule ThundermoonWeb.ErrorHTMLTest do
   use ThundermoonWeb.ConnCase, async: true
 
   # Bring render/3 and render_to_string/3 for testing custom views
-  import Phoenix.View
+  import Phoenix.Template
 
   alias ThundermoonWeb.ErrorHTML
 
   test "renders 404.html" do
-    assert render_to_string(ErrorHTML, "404.html", []) == "Not Found"
+    assert render_to_string(ErrorHTML, "404", "html", []) == "Not Found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(ErrorHTML, "500.html", []) == "Internal Server Error"
+    assert render_to_string(ErrorHTML, "500", "html", []) == "Internal Server Error"
   end
 end
