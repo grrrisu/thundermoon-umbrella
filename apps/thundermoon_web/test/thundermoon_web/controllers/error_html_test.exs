2d1e98b12e9f3282250e9eb31deb8c0a842e0854
Alessandro Di Maria <adm@m42.ch>
2023-03-17 16:05:37 +0100

    flash alert component

--- apps/thundermoon_web/test/thundermoon_web/controllers/error_html_test.exs
+++ apps/thundermoon_web/test/thundermoon_web/controllers/error_html_test.exs
@@ -4,11 +4,13 @@ defmodule ThundermoonWeb.ErrorHTMLTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  alias ThundermoonWeb.ErrorHTML
+
   test "renders 404.html" do
-    assert render_to_string(ThundermoonWeb.ErrorHTML, "404.html", []) == "Not Found"
+    assert render_to_string(ErrorHTML, "404.html", []) == "Not Found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(ThundermoonWeb.ErrorHTML, "500.html", []) == "Internal Server Error"
+    assert render_to_string(ErrorHTML, "500.html", []) == "Internal Server Error"
   end
 end
