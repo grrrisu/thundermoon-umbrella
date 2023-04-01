c5cdf00af683fbeb197ae05054d38682812e5f8d
Alessandro Di Maria <adm@m42.ch>
2023-04-01 13:59:01 +0200

    hero section

--- apps/thundermoon_web/lib/thundermoon_web/controllers/page_html.ex
+++ apps/thundermoon_web/lib/thundermoon_web/controllers/page_html.ex
@@ -2,4 +2,16 @@ defmodule ThundermoonWeb.PageHTML do
   use ThundermoonWeb, :html
 
   embed_templates "page_html/*"
+
+  attr :title, :string, required: true
+  slot :inner_block
+
+  def hero_section(assigns) do
+    ~H"""
+    <section class="bg-gray-700 my-8 p-8 rounded-lg shadow-lg text-center">
+      <h1 class="text-4xl font-semibold mb-4"><%= @title %></h1>
+      <p :if={@inner_block}><%= render_slot(@inner_block) %></p>
+    </section>
+    """
+  end
 end
